6f9c4b1eb7a487bcf93e2008f1e49322629aa44a
Toluwanimi Alfred <alfredfadipe09@gmail.com>
2026-04-24 17:19:57 +0100

    Implemented frame-rate independent gravity simulation using SDL2  - Added delta time based physics - Introduced velocity integration - Implemented ground collision clamp - Switched to float-based position for smooth motion - Enabled VSYNC for stable rendering

--- gravity_learn.c
+++ gravity_learn.c
@@ -1,62 +1,68 @@
 #include<stdio.h>
-#include<stdbool.h>
 #include<SDL2/SDL.h>
+#include<stdbool.h>
 
 #define WIDTH 800
-#define HEIGHT 400
+#define HEIGHT 700
 
 int main(int argc, char *argv[]){
     float gravity = 800.0f;
     float velocityY = 0.0f;
+    float playerY = 250.0f;
 
-    SDL_Rect Player = {0, 0, 20, 50};
+    SDL_Rect Player = {0, 250, 30, 50};
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow(
-        "Gravity Simulation", 
+    SDL_Window *window = SDL_CreateWindow("GRAVITY SIMULATION", 
         SDL_WINDOWPOS_CENTERED, 
         SDL_WINDOWPOS_CENTERED, 
         WIDTH, 
         HEIGHT, 
-        0);
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        SDL_WINDOW_SHOWN);
+    
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     bool running = true;
     SDL_Event event;
-    Uint32 last_time = SDL_GetTicks();
 
+    Uint32 last_time = SDL_GetTicks();
     while(running){
         Uint32 current_time = SDL_GetTicks();
         float delta_time = (current_time - last_time) / 1000.0f;
         last_time = current_time;
-
-        while(SDL_PollEvent(&event)){
+        while (SDL_PollEvent(&event))
+        {
             if(event.type == SDL_QUIT){
                 running = false;
             }
         }
+    
         velocityY += gravity * delta_time;
-        Player.y += (int)(velocityY * delta_time);
-
+        
+        playerY  += (velocityY * delta_time);
+        
         int ground = HEIGHT - 50;
-        if(Player.y + Player.h >= ground){
-            Player.y = ground - Player.h;
+
+        if(playerY + Player.h >= ground){
+            playerY = ground - Player.h;
             velocityY = 0.0f;
         }
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        Player.y = (int)playerY;
+        
+        SDL_SetRenderDrawColor(renderer, 0, 0, 180, 255);
         SDL_RenderClear(renderer);
-
-        SDL_SetRenderDrawColor(renderer, 200, 50, 50, 255);
+        
+        SDL_SetRenderDrawColor(renderer, 20, 255 , 0, 240);
         SDL_Rect groundRect = {0, ground, WIDTH, 50};
         SDL_RenderFillRect(renderer, &groundRect);
 
-        SDL_SetRenderDrawColor(renderer, 200, 50, 50, 255);
+        SDL_SetRenderDrawColor(renderer, 230, 0, 0, 255);
         SDL_RenderFillRect(renderer, &Player);
 
-    SDL_RenderPresent(renderer);
+        SDL_RenderPresent(renderer);
+        
     }
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
